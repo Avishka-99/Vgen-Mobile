9e770523a5b57122d65a7597724850dfdfc5f2a7
shanuDhanusha <2020cs186@stu.ucsc.cmb.ac.lk>
2023-07-29 20:54:29 +0530

    make new profile update

--- client/constants/Constants.js
+++ client/constants/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://192.168.51.56';
+export const BASE_URL = 'http://192.168.231.56';
 export const PORT = '5001';
 export const PRODUCT_IMG_PATH = '/Vgen-Server/uploads/products/';
 export const RESTAURANT_IMG_PATH = '/uploads/restaurants/';
