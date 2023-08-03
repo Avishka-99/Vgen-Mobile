bf8253e749ad84d175ad5e9e0c1e4493e0e37bf7
shanuDhanusha <2020cs186@stu.ucsc.cmb.ac.lk>
2023-08-03 08:12:50 +0530

    home pqge

--- client/components/Header.js
+++ client/components/Header.js
@@ -29,7 +29,7 @@ const  styles=StyleSheet.create({
        flexDirection:'row',
        shadowOffset:{width:4,height:4},
        shadowColor:'#EFF6F1',
-       marginTop:0
+       
       // backgroundColor:'red'
     },
     imageBacbutton:{
