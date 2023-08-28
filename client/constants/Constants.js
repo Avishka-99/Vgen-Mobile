3ebd4495be30147f6237a6a281483aac7b7b23a9
Avishka Prabhath <avishkaprabha360@gmail.com>
2023-08-28 07:58:10 +0530

    changes

--- client/constants/Constants.js
+++ client/constants/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://10.22.127.236';
+export const BASE_URL = 'http://10.22.164.207';
 export const PORT = '5001';
 export const PRODUCT_IMG_PATH = '/uploads/products/';
 export const RESTAURANT_IMG_PATH = '/uploads/restaurants/';
