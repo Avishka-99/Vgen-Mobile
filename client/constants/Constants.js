f36263bdbff448b5afd546c24a8a85b1174ffe56
shanuDhanusha <2020cs186@stu.ucsc.cmb.ac.lk>
2023-10-30 20:59:29 +0530

    changes

--- client/constants/Constants.js
+++ client/constants/Constants.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export const BASE_URL = 'http://192.168.125.56';
-=======
-export const BASE_URL = 'http://10.22.54.110';
->>>>>>> 319c9cb1374806b16c0143a0619e3d35da02c985
 export const PORT = '5001';
 export const PRODUCT_IMG_PATH = '/uploads/products/';
 export const RESTAURANT_IMG_PATH = '/uploads/restaurants/';
